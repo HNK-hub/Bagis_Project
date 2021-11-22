f7032fd267cd20e65371420780e7f229c96bab93
Hatice Nur KAYA <66225075+HNK-hub@users.noreply.github.com>
2021-11-22 21:16:30 +0300

    Delete bagisProject directory

--- bagisProject/src/main/java/com/takim4/bagisProject/dataAccess/abstracts/InstitutionsDao.java
+++
@@ -1,9 +0,0 @@
-package com.takim4.bagisProject.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.takim4.bagisProject.entities.concretes.Institutions;
-
-public interface InstitutionsDao extends JpaRepository<Institutions, Integer>{
-
-}
--- bagisProject/src/main/java/com/takim4/bagisProject/dataAccess/abstracts/SpecialDayDao.java
+++
@@ -1,8 +0,0 @@
-package com.takim4.bagisProject.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.takim4.bagisProject.entities.concretes.SpecialDay;
-
-public interface SpecialDayDao extends JpaRepository<SpecialDay, Integer>{
-
-}
